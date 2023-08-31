44fd127e996846e24e082659c0b221fd2800b93d
Kiiza Atwiine Stephen <atwiinestephen123@gmail.com>
2023-08-31 16:23:38 +0300

    sidebar and cart completed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,8 @@ import { CartContext } from "../contexts/CartContext";
 
 export default function Sidebar() {
 	const { sidebarOpen, openSidebar } = React.useContext(SidebarContext);
-	const { cart, clearCart } = React.useContext(CartContext);
+	const { cart, clearCart, totalPrice, setTotalPrice } =
+		React.useContext(CartContext);
 	return (
 		<React.Fragment>
 			<div
@@ -27,9 +28,9 @@ export default function Sidebar() {
 						<IoMdArrowForward className="text-2xl" />
 					</div>
 				</div>
-				<div>
+				<div className="flex flex-col gap-y-2 h-[520px] lg:h-[650px] overflow-y-auto overflow-x-hidden border-b">
 					{cart.map((item) => (
-						<div>
+						<div key={item.id}>
 							<CartItem key={item.id} item={item} />
 						</div>
 					))}
@@ -38,7 +39,8 @@ export default function Sidebar() {
 					<div className="flex w-full justify-between items-center">
 						{/* total */}
 						<div className="uppercase font-semibold">
-							<span className="mr-2">Total: </span>$ 1000
+							<span className="mr-2">Total: </span>${" "}
+							{parseFloat(totalPrice).toFixed(2)}
 						</div>
 						{/* clear cart icon */}
 						<div className="cursor-pointer py-4 bg-red-500 text-white w-12 h-12 flex justify-center items-center text-xl">
--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -6,6 +6,30 @@ export const CartContext = React.createContext();
 // cart context provider
 export const CartProvider = ({ children }) => {
 	const [cart, setCart] = React.useState([]);
+	
+	// set cart count state
+	const [cartCount, setCartCount] = React.useState(0);
+
+	// total price state
+	const [totalPrice, setTotalPrice] = React.useState(0);
+
+	// update total price
+	React.useEffect(() => {
+		const total = cart.reduce((acc, item) => {
+			return acc + item.price * item.amount;
+		}, 0);
+		setTotalPrice(total);
+	}); 
+
+	// update cart count
+	React.useEffect(() => {
+		if(cart) {
+			const amount = cart.reduce((acc, item) => {
+				return acc + item.amount;
+			}, 0);
+			setCartCount(amount);
+		}
+	}, [cart]);
 
 	const addToCart = (product, id) => {
 		const newItem = { ...product, amount: 1 };
@@ -37,9 +61,44 @@ export const CartProvider = ({ children }) => {
 		setCart([]);
 	};
 
+	const increaseItemCount = (id) => {
+		const item = cart.find((item) => item.id === id);
+		addToCart(item, id);
+	};
+
+	const decreaseItemCount = (id) => {
+		const item = cart.find((item) => {
+			return item.id === id;
+		});
+		if (item) {
+			const newCart = cart.map((item) => {
+				if (item.id === id) {
+					return { ...item, amount: item.amount - 1 };
+				} else {
+					return item;
+				}
+			});
+			setCart(newCart);
+		}
+		if (item.amount < 2) {
+			removeFromCart(id);
+		}
+	};
+
 	return (
 		<CartContext.Provider
-			value={{ cart, addToCart, removeFromCart, clearCart }}>
+			value={{
+				cart,
+				increaseItemCount,
+				decreaseItemCount,
+				addToCart,
+				removeFromCart,
+				clearCart,
+				cartCount,
+				setCartCount,
+				totalPrice,
+				setTotalPrice
+			}}>
 			{children}
 		</CartContext.Provider>
 	);
