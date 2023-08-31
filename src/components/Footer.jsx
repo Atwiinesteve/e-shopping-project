edc39bcde9da7084b3485a892b571ad092ac6c9b
Kiiza Atwiine Stephen <atwiinestephen123@gmail.com>
2023-08-31 17:16:52 +0300

    project completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
-  return (
-    <React.Fragment>
-      <footer className="bg-primary py-10">
-        <div className="container mx-auto">
-          <p className="text-white text-center">Copyright &copy; Ecommerce Shop 2023. All rights reserved</p>
-        </div>
-      </footer>
-    </React.Fragment>
-  )
+	return (
+		<React.Fragment>
+			<footer className="bg-primary py-10">
+				<div className="container mx-auto">
+					<p className="text-white text-center">
+						Copyright &copy; Ecommerce Shop 2023. All rights reserved
+					</p>
+				</div>
+			</footer>
+		</React.Fragment>
+	);
 }
